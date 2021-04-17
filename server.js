55e651d37c952370343025abb535695ece99ab26
beckie fang <beckie_fang@worldvision.org.tw>
2021-04-17 22:31:41 +0800

    update pipeline

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
+  res.send('sample nodejs app using azure pipeline!\n');
 });
 
 var port = process.env.PORT||PORT;
